0753a2ff3243781abc5c1bce007fb46f94d715d8
Dmitriy Zheltyi <dazheltyy@gmail.com>
2022-04-25 21:12:55 +0300

    refactor: провел рефакторинг кода

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 
-# el = new Element()
 import os
 from app.utils.env import load_env
-from app.core.element import *
+from app.core.application import Application
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 
 def main():
 	env_vars = load_env(dotenv_path)
 
-	el = Element(env_vars)
-	el.start()
+	Application(env_vars).start()
 
 if __name__ == "__main__":
-    main()
-
-# window = sg.Window('Авто-кликер', layout)
+	main()
