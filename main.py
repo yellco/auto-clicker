c9fd60fed27824196c795871f67261fee85b6cc5
ZheltyiDA <zheltyida@pgk.ru>
2022-04-27 18:28:32 +0300

    fix: добавил проверку на наличие файла

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ from app.core.application import Application
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 
 def main():
+	"""
+	Точка входа приложения
+	"""
 	env_vars = load_env(dotenv_path)
 
 	Application(env_vars).start()
 
 if __name__ == "__main__":
+	if not os.path.exists("./.env"):
+		raise FileNotFoundError("Отсутствие .env файла")
 	main()
